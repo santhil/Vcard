343ca437e56c16ca9f7857c4f05724164232e93c
Senthil.Gunasekaran <Senthil.Gunasekaran@KANINI-LTP-877>
2022-12-23 17:59:08 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Vcard;
+
+var myCard = new VCard
+{
+    FirstName = "Senthil Kumar",
+    LastName = "G",
+    Organization = "Kanini",
+    JobTitle = "Title of Job",
+    StreetAddress = "Tööstuse 48",
+    City = "Tallinn",
+    CountryName = "Estonia",
+    Phone = "343-23232",
+    Mobile = "232-67854",
+    Email = "gunnar@secret-address.com",
+    HomePage = "www.developers-team.com"
+};
+myCard.Image = File.ReadAllBytes("vcardphoto.png");
+using (var file = File.OpenWrite("vcardphoto.vcf"))
+using (var writer = new StreamWriter(file))
+{
+    writer.Write(myCard.ToString());
+}
+
+
---
+++ Vcard.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vcard
+{
+    public class VCard
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Organization { get; set; }
+        public string JobTitle { get; set; }
+        public string StreetAddress { get; set; }
+        public string Zip { get; set; }
+        public string City { get; set; }
+        public string CountryName { get; set; }
+        public string Phone { get; set; }
+        public string Mobile { get; set; }
+        public string Email { get; set; }
+        public string HomePage { get; set; }
+        public byte[] Image { get; set; }
+        public override string ToString()
+        {
+            var builder = new StringBuilder();
+            builder.AppendLine("BEGIN:VCARD");
+            builder.AppendLine("VERSION:2.1");
+            // Name
+            builder.AppendLine("N:" + LastName + ";" + FirstName);
+            // Full name
+            builder.AppendLine("FN:" + FirstName + " " + LastName);
+            // Address
+            builder.Append("ADR;HOME;PREF:;;");
+            builder.Append(StreetAddress + ";");
+            builder.Append(City + ";;");
+            builder.Append(Zip + ";");
+            builder.AppendLine(CountryName);
+            // Other data
+            builder.AppendLine("ORG:" + Organization);
+            builder.AppendLine("TITLE:" + JobTitle);
+            builder.AppendLine("TEL;HOME;VOICE:" + Phone);
+            builder.AppendLine("TEL;CELL;VOICE:" + Mobile);
+            builder.AppendLine("URL;" + HomePage);
+            builder.AppendLine("EMAIL;PREF;INTERNET:" + Email);
+            builder.AppendLine("PHOTO;ENCODING=BASE64;TYPE=JPEG:"+ Convert.ToBase64String(Image));
+            
+            builder.AppendLine("END:VCARD");
+            return builder.ToString();
+        }
+    }
+}
